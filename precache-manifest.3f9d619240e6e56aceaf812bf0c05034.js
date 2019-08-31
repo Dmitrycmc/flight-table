5a1ee93f9aa4fbc375c94c8609366e90b2aed7d8
Dmitry Lytov <dmitrycmc@gmail.com>
2019-08-31 05:03:24 +0300

    Updates

---
+++ precache-manifest.3f9d619240e6e56aceaf812bf0c05034.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "bd26a56dbeb1e7e41dd1",
+    "url": "/flight-table/static/js/runtime~main.f11e6b52.js"
+  },
+  {
+    "revision": "ac233c27dc287d89c69d",
+    "url": "/flight-table/static/js/main.491da24c.chunk.js"
+  },
+  {
+    "revision": "ac233c27dc287d89c69d",
+    "url": "/flight-table/static/css/main.4ab9e11b.chunk.css"
+  },
+  {
+    "revision": "0b99aac7f04ca06bebed33d5df51271a",
+    "url": "/flight-table/index.html"
+  }
+];
